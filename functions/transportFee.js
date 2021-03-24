1b22959be2c87a15c483599dba88e81ea4d906db
Amanda <amandamaarman19911@gmail.com>
2021-03-24 11:58:09 +0200

    tests files renamed

--- functions/transportFee.js
+++ functions/transportFee.js
@@ -1,9 +1,9 @@
 function transportFee(shift){
 	if(shift === "morning"){
-    	return "R20"
+    	return "R20";
     }
   else if(shift === "afternoon"){
-  	return "R10"
+  	return "R10";
   }
   else{
   	return "free";
--- tests/transportFee.test.js
+++ tests/transportFee.test.js
@@ -5,7 +5,7 @@ describe('this function returns the right price based on the shift you are worki
     it('if I am working afternoon shift I will pay', function(){
         assert.equal("R10.00" ,totalPhoneBill("afternoon"));
     });
-    it('if I am working late shift I will nothing because work provides transport', function(){
-        assert.equal("R0.00" ,totalPhoneBill("other"));
+    it('if I am working late shift I will pay nothing because work provides transport', function(){
+        assert.equal("R0.00" ,totalPhoneBill("late"));
     })
 })
